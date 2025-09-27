8899057d578768fedad6630c328fe08cc8c6ee2d
hmilezx <130328073+hmilezx@users.noreply.github.com>
2025-09-27 22:13:23 +1200

    adding two numbers

--- contributions.js
+++ contributions.js
@@ -1,3 +1,10 @@
 // welcome to my javascript journey 
 // kubu tolo 
 // another day another contribution 
+
+// start off witrh a basic function 
+// adding two numbers
+
+function addNumbers(numberRua, numberDua) {
+    return numberRua + numberDua;
+}
