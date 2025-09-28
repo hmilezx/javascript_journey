f916fbe7c53a65a2e17f4d6afcd76e94d98461e4
hmilezx <130328073+hmilezx@users.noreply.github.com>
2025-09-28 12:44:55 +1200

    just learning how to code

--- contributions.js
+++ contributions.js
@@ -1,10 +1,71 @@
-// welcome to my javascript journey 
-// kubu tolo 
-// another day another contribution 
+// // welcome to my javascript journey 
+// // kubu tolo 
+// // another day another contribution 
 
-// start off witrh a basic function 
-// adding two numbers
+// // start off witrh a basic function 
+// // adding two numbers
 
-function addNumbers(numberRua, numberDua) {
-    return numberRua + numberDua;
+// function addNumbers(numberRua, numberDua) {
+//     return numberRua + numberDua;
+// }
+
+// function checkifEven(number)
+// {
+//     if (number % 2 === 0) {
+//         return true;
+//     }
+
+//     else {
+//         return number + 1;
+//     }
+// }
+
+
+// console.log(checkifEven(3));
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+let Name;
+
+function imthatGuy(name) {
+    const normalizedName = name.toLowerCase();
+     Name = name;
+    try{
+        
+        if (normalizedName === "miles")
+            console.log("You are that nigga");
+        else
+            console.log("kys"); 
+        
+    }
+    finally{
+        return "${Name} was here";
+
+    }
+
 }
+function askName() {
+rl.question("what is your name nigga? (type exit to quit): ", (answer) => {
+    if (answer.toLowerCase() === "exit") {
+        console.log("goodbye nigga");
+        rl.close();
+        return;
+    }
+    imthatGuy(answer);
+    askName();
+    // rl.close();
+});
+}
+
+askName();
+
+
+
+
+
