3efb79c836d9ec1fc2348800fb821c1eff193bd0
hmilezx <hxmilez@gmail.com>
2025-10-17 16:17:05 +1200

    this is for my github contributions

--- contributions.js
+++ contributions.js
@@ -1,70 +1,73 @@
-// // welcome to my javascript journey 
-// // kubu tolo 
-// // another day another contribution 
+// // // welcome to my javascript journey 
+// // // kubu tolo 
+// // // another day another contribution 
 
-// // start off witrh a basic function 
-// // adding two numbers
+// // // start off witrh a basic function 
+// // // adding two numbers
 
-// function addNumbers(numberRua, numberDua) {
-//     return numberRua + numberDua;
-// }
+// // function addNumbers(numberRua, numberDua) {
+// //     return numberRua + numberDua;
+// // }
 
-// function checkifEven(number)
-// {
-//     if (number % 2 === 0) {
-//         return true;
+// // function checkifEven(number)
+// // {
+// //     if (number % 2 === 0) {
+// //         return true;
+// //     }
+
+// //     else {
+// //         return number + 1;
+// //     }
+// // }
+
+
+// // console.log(checkifEven(3));
+
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+
+// let Name;
+
+// function imthatGuy(name) {
+//     const normalizedName = name.toLowerCase();
+//      Name = name;
+//     try{
+        
+//         if (normalizedName === "miles" || normalizedName === "joyce" || normalizedName === "cyrus")
+//             console.log("You are that nigga");
+//         else
+//             console.log("kys"); 
+        
+//     }
+//     finally{
+//         return console.log(`${Name} was here`); // I forgot to add console log earlier so it ended up just returning smth which was not used 
 //     }
 
-//     else {
-//         return number + 1;
+// }
+// function askName() {
+// rl.question("what is your name nigga? (type exit to quit): ", (answer) => {
+//     if (answer.toLowerCase() === "exit") {
+//         console.log("goodbye nigga");
+//         rl.close();
+//         return;
 //     }
+//     imthatGuy(answer);
+//     askName();
+//     // rl.close();
+// });
 // }
 
-
-// console.log(checkifEven(3));
-
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// askName();
 
 
-let Name;
 
-function imthatGuy(name) {
-    const normalizedName = name.toLowerCase();
-     Name = name;
-    try{
-        
-        if (normalizedName === "miles" || normalizedName === "joyce" || normalizedName === "cyrus")
-            console.log("You are that nigga");
-        else
-            console.log("kys"); 
-        
-    }
-    finally{
-        return console.log(`${Name} was here`); // I forgot to add console log earlier so it ended up just returning smth which was not used 
-    }
 
+function chatAmIcooked(answer) {
+    
 }
-function askName() {
-rl.question("what is your name nigga? (type exit to quit): ", (answer) => {
-    if (answer.toLowerCase() === "exit") {
-        console.log("goodbye nigga");
-        rl.close();
-        return;
-    }
-    imthatGuy(answer);
-    askName();
-    // rl.close();
-});
-}
-
-askName();
-
-
-
-
 
